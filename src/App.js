6612e568d59199307825c16c35f1dabf99b79535
Kd125 <kashdangar1250@gmail.com>
2023-05-22 23:11:49 -0500

    complete project

--- src/App.js
+++ src/App.js
@@ -1,25 +1,106 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import React, { Component } from "react";
+import axios from "axios";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      searchWord: "",
+      date: "",
+      author: "",
+      articles: [],
+    };
+  }
+
+  onSearch = (e) => {
+    this.setState({ searchWord: e.target.value });
+  };
+
+  onDate = (e) => {
+    this.setState({ date: e.target.value });
+  };
+
+  onAuthor = (e) => {
+    this.setState({ author: e.target.value });
+  };
+
+  onSubmit = (e) => {
+    e.preventDefault();
+    axios
+      .get(
+        `https://hn.algolia.com/api/v1/search?query=${this.state.searchWord}`
+      )
+      .then((response) => {
+        this.setState({ articles: response.data.hits });
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  };
+
+  onSubmitAuthDate = (e) => {
+    const { author, date } = this.state;
+    e.preventDefault();
+    let apiUrl = "https://hn.algolia.com/api/v1/search?";
+    if (author) {
+      apiUrl += `query=${author}&tags=story`;
+    }
+    if (date) {
+      apiUrl += `numericFilters=created_at_i>${
+        Date.parse(date) / 1000
+      }&tags=story`;
+    }
+    axios
+      .get(apiUrl)
+      .then((response) => {
+        this.setState({ articles: response.data.hits });
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  };
+
+  render() {
+    return (
+      <div className="App">
+        <div className="header">
+          <form onSubmit={this.onSubmit}>
+            <input
+              className="searchBox"
+              type="text"
+              placeholder="Search"
+              onChange={this.onSearch}
+            />
+            <input type="submit" value="Search" />
+          </form>
+          <form onSubmit={this.onSubmitAuthDate}>
+            <input
+              className="searchBox"
+              type="text"
+              placeholder="Author"
+              onChange={this.onAuthor}
+            />
+            <input
+              className="searchBox"
+              type="text"
+              placeholder="Date"
+              onChange={this.onDate}
+            />
+            <input type="submit" value="Search" />
+          </form>
+        </div>
+        <ul>
+          {this.state.articles.map((item) => (
+            <li className="search-list" key={item.objectID}>
+              <a href={item.url}>{item.title}</a> - {item.author}
+            </li>
+          ))}
+        </ul>
+      </div>
+    );
+  }
 }
 
 export default App;
